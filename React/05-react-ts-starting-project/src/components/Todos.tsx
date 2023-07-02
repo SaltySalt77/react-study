3246fd5c51fe0f794f5f2b8ecb2a9d24d8ddf95f
SaltySalt77 <alexisna@naver.com>
2023-07-02 14:12:26 +0900

    feat : added TodoItem component

--- React/05-react-ts-starting-project/src/components/Todos.tsx
+++ React/05-react-ts-starting-project/src/components/Todos.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]}> = (props) => {
   return (<ul>
-    {props.items.map((item) => <li key={item}>{item}</li>)}
+    {props.items.map((item) => {return <TodoItem  key={item.id} text={item.text} />})}
   </ul>);
 }
 
